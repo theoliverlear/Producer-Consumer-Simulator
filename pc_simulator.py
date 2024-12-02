0ebb297bb662f4207daedff82619763e6f6e60e7
theoliverlear <sigwartholiver2@gmail.com>
2024-12-01 19:12:33 -0600

    Begin main method implementation with initialization of Simulator object from command line arguments in pc_simulator.py

--- pc_simulator.py
+++ pc_simulator.py
@@ -1,5 +1,14 @@
+import sys
+
+from src.main.config.command_parser import get_config_from_arguments
+from src.main.config.config import Config
+from src.main.simulator.simulator import Simulator
+
+
 def main():
-    pass
+    config: Config = get_config_from_arguments(sys.argv[1:])
+    simulator: Simulator = Simulator(config)
+    simulator.simulate()
 
 
 if __name__ == "__main__":
