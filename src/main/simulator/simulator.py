648801508962cd34ce66d7509a41c8ab564ffbc5
theoliverlear <sigwartholiver2@gmail.com>
2024-12-01 19:08:19 -0600

    Add initial structure and methods to Simulator class in simulator.py for future implementations

--- src/main/simulator/simulator.py
+++ src/main/simulator/simulator.py
@@ -1,3 +1,38 @@
+from src.main.config.config import Config
+
+
 class Simulator:
     # Thread Manager (num_producers), (num_consumers), (buffer_size)
-    pass
+    """
+    Pipe in args
+    Check validity
+    Get commands
+    Initialize lock
+    Initialize thread manager
+        - Initialize producer amount and speed
+        - Initialize consumer amount and speed
+
+
+
+
+    """
+    def __init__(self, config: Config):
+        self.config = config
+
+    def simulate(self):
+        self.start()
+        # TODO: Add business logic
+        self.stop()
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def show_suggestions(self):
+        pass
+
+    def show_efficiency(self):
+        pass
+
