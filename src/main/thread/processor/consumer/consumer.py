34ccd55f96691e6195a7e7c1a45bdae08b293134
theoliverlear <sigwartholiver2@gmail.com>
2024-12-01 19:03:20 -0600

    Add implementation of Consumer class with buffer handling and processing logic in consumer.py

--- src/main/thread/processor/consumer/consumer.py
+++ src/main/thread/processor/consumer/consumer.py
@@ -1,2 +1,22 @@
-class Consumer:
-    pass
+from abc import ABC
+
+from src.main.buffer.buffer import Buffer
+from src.main.buffer.empty_buffer_exception import EmptyBufferException
+from src.main.thread.processor.processor import Processor
+
+
+class Consumer(Processor, ABC):
+    def __init__(self, speed_floor: int, speed_ceiling: int, buffer: Buffer):
+        super().__init__(speed_floor, speed_ceiling, buffer)
+
+    def run(self) -> None:
+        while self.running:
+            try:
+                self.buffer.dequeue()
+            except EmptyBufferException:
+                pass
+            super().simulate_processing()
+
+
+    def stop(self):
+        self.running = False
