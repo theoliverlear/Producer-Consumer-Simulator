c91ad5add196d3db413a3ac27fe7d869447f5aba
theoliverlear <sigwartholiver2@gmail.com>
2024-12-02 08:53:36 -0600

    Integrate BufferQueue and ThreadManager setup with logging in Simulator class in simulator.py

--- src/main/simulator/simulator.py
+++ src/main/simulator/simulator.py
@@ -1,23 +1,22 @@
+from src.main.buffer.buffer_queue import BufferQueue
 from src.main.config.config import Config
+from src.main.logging.setup_logging import setup_logging
+from src.main.thread.thread_manager import ThreadManager
 
 
 class Simulator:
-    # Thread Manager (num_producers), (num_consumers), (buffer_size)
-    """
-    Pipe in args
-    Check validity
-    Get commands
-    Initialize lock
-    Initialize thread manager
-        - Initialize producer amount and speed
-        - Initialize consumer amount and speed
-
-
-
-
-    """
     def __init__(self, config: Config):
         self.config = config
+        setup_logging(config.verbose)
+        self.buffer = BufferQueue(config.buffer_size, config.num_items_to_process)
+        self.thread_manager = ThreadManager(
+            num_producers=config.num_producers,
+            num_consumers=config.num_consumers,
+            consumer_speed_range=config.consumer_speed_range,
+            producer_speed_range=config.producer_speed_range,
+            buffer=self.buffer
+        )
+        self.is_running = False
 
     def simulate(self) -> None:
         self.start()
@@ -25,10 +24,12 @@ class Simulator:
         self.stop()
 
     def start(self) -> None:
-        pass
+        self.is_running = True
+        self.thread_manager.start_all()
 
     def stop(self) -> None:
-        pass
+        self.is_running = False
+        self.thread_manager.stop_all()
 
     def show_suggestions(self) -> None:
         pass
