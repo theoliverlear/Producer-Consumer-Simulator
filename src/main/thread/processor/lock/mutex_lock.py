2494d2953fbc50305a5b14b452cafa18364a384f
theoliverlear <sigwartholiver2@gmail.com>
2024-12-02 16:24:52 -0600

    Refactor MutexLock class to add queue mechanism for waiting threads and add logging in mutex_lock.py

--- src/main/thread/processor/lock/mutex_lock.py
+++ src/main/thread/processor/lock/mutex_lock.py
@@ -1,4 +1,8 @@
+import itertools
+import logging
 import threading
+from threading import Condition
+from typing import List
 
 from src.main.thread.processor.lock.lock_state import LockState
 
@@ -6,17 +10,47 @@ from src.main.thread.processor.lock.lock_state import LockState
 class MutexLock:
     def __init__(self):
         self.lock_state: LockState = LockState.AVAILABLE
-        self.lock: threading.Lock = threading.Lock()
+        self.condition: Condition = threading.Condition()
+        self.queue_counter: itertools.count[int] = itertools.count()
+        self.wait_queue: List[int] = []
 
-    def acquire(self):
-        self.lock.acquire()
+    def get_next_queue_number(self) -> int:
+        return next(self.queue_counter)
+
+
+    def lock(self):
         self.lock_state = LockState.UNAVAILABLE
 
-    def release(self):
+    def unlock(self):
         self.lock_state = LockState.AVAILABLE
-        self.lock.release()
 
-    def is_available(self):
+    def is_locked(self) -> bool:
+        return self.lock_state == LockState.UNAVAILABLE
+
+    def is_thread_at_front(self, wait_number: int) -> bool:
+        return len(self.wait_queue) > 0 and self.wait_queue[0] == wait_number
+
+
+    def acquire(self) -> None:
+        with self.condition:
+            wait_number: int = self.get_next_queue_number()
+            self.wait_queue.append(wait_number)
+            thread_name: str = threading.current_thread().name
+            logging.debug(f"Thread {thread_name} requesting the lock with wait number {wait_number}.")
+            while self.is_locked() or not self.is_thread_at_front(wait_number):
+                self.condition.wait()
+            self.wait_queue.pop(0)
+            self.lock()
+            logging.debug(f"Thread {thread_name} acquired the lock.")
+
+
+    def release(self) -> None:
+        with self.condition:
+            self.unlock()
+            logging.debug(f"Thread {threading.current_thread().name} released the lock.")
+            self.condition.notify_all()
+
+    def is_available(self) -> bool:
         return self.lock_state == LockState.AVAILABLE
 
 
