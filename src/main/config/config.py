94299dbf6101402ba0587fb845fafba221a7a58a
theoliverlear <sigwartholiver2@gmail.com>
2024-12-02 08:47:54 -0600

    Add parameters to Config constructor in config.py to implement configuration functionality

--- src/main/config/config.py
+++ src/main/config/config.py
@@ -1,3 +1,18 @@
 class Config:
-    def __init__(self):
-        pass
+    def __init__(self,
+                 buffer_size: int,
+                 num_items_to_process: int,
+                 num_producers: int,
+                 num_consumers: int,
+                 consumer_speed_range: tuple[int, int],
+                 producer_speed_range: tuple[int, int],
+                 verbose: bool,
+                 suggestions: bool):
+        self.buffer_size = buffer_size
+        self.num_items_to_process = num_items_to_process
+        self.num_producers = num_producers
+        self.num_consumers = num_consumers
+        self.consumer_speed_range = consumer_speed_range
+        self.producer_speed_range = producer_speed_range
+        self.verbose = verbose
+        self.suggestions = suggestions
