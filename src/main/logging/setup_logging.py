ffaeee39e3b5d08f9867ba8299c7d53789d85575
theoliverlear <sigwartholiver2@gmail.com>
2024-12-02 16:18:21 -0600

    Refactor setup_logging.py to separate verbose and default logging configurations and move performance_timing function to its own file

--- src/main/logging/setup_logging.py
+++ src/main/logging/setup_logging.py
@@ -3,10 +3,20 @@ import logging
 import threading
 import time
 
-
-def setup_logging(use_verbose: bool = False):
+def setup_logging(use_verbose: bool = False) -> None:
+    if use_verbose:
+        setup_verbose_logging()
+    else:
+        setup_default_logging()
+def setup_default_logging() -> None:
     logging.basicConfig(
-        level=logging.DEBUG if use_verbose else logging.INFO,
+        level=logging.INFO,
+        format="%(asctime)s - %(threadName)s - %(levelname)s - %(message)s"
+    )
+
+def setup_verbose_logging():
+    logging.basicConfig(
+        level=logging.DEBUG,
         format="%(asctime)s - %(threadName)s - %(levelname)s - %(message)s"
     )
 
@@ -19,14 +29,4 @@ def execution_trace(function):
         return result
     return wrapper
 
-def performance_timing(function):
-    @functools.wraps(function)
-    def wrapper(*args, **kwargs):
-        thread_name: str = threading.current_thread().name
-        start_time: float = time.perf_counter()
-        result = function(*args, **kwargs)
-        end_time: float = time.perf_counter()
-        execution_time: float = end_time - start_time
-        logging.debug(f"Execution time for {function.__name__} on thread {thread_name}: {execution_time:.4f} seconds")
-        return result
-    return wrapper
+
