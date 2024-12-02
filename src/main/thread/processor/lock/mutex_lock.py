78194ac216fb26a232dcd3552595166a38b92d8b
theoliverlear <sigwartholiver2@gmail.com>
2024-12-01 19:06:11 -0600

    Add MutexLock implementation with threading for lock management in mutex_lock.py

--- src/main/thread/processor/lock/mutex_lock.py
+++ src/main/thread/processor/lock/mutex_lock.py
@@ -1,2 +1,27 @@
+import threading
+
+from src.main.thread.processor.lock.lock_state import LockState
+
+
 class MutexLock:
-    pass
+    def __init__(self):
+        self.lock_state: LockState = LockState.AVAILABLE
+        self.lock: threading.Lock = threading.Lock()
+
+    def acquire(self):
+        self.lock.acquire()
+        self.lock_state = LockState.UNAVAILABLE
+
+    def release(self):
+        self.lock_state = LockState.AVAILABLE
+        self.lock.release()
+
+    def is_available(self):
+        return self.lock_state == LockState.AVAILABLE
+
+
+    def __enter__(self):
+        self.acquire()
+
+    def __exit__(self, exception_type, exception_value, exception_traceback):
+        self.release()
