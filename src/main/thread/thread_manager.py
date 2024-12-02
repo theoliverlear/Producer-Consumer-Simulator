6c815c76e72237a56de5bb1711e2b4126bf3c753
theoliverlear <sigwartholiver2@gmail.com>
2024-12-01 19:16:58 -0600

    Add initialization of producer and consumer processors in ThreadManager class in thread_manager.py

--- src/main/thread/thread_manager.py
+++ src/main/thread/thread_manager.py
@@ -1,2 +1,16 @@
+from src.main.thread.processor.processor import Processor
+
+
 class ThreadManager:
-    pass
+    def __init__(self, num_producers: int,
+                 num_consumers: int,
+                 consumer_speed_range: tuple[int, int],
+                 producer_speed_range: tuple[int, int]):
+        self.producers = self.initialize_processors(num_producers, producer_speed_range)
+        self.consumers = self.initialize_processors(num_consumers, consumer_speed_range)
+
+    @staticmethod
+    def initialize_processors(num_processors, processor_speed_range):
+        return [Processor(processor_speed_range[0], processor_speed_range[1]) for
+                i in range(num_processors)]
+
