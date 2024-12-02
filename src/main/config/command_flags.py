2f4a6ec8d0167334ecf13d3deb983a4e789face0
theoliverlear <sigwartholiver2@gmail.com>
2024-12-01 18:58:09 -0600

    Add command flags used in the program to CommandFlags enum in command_flags.py

--- src/main/config/command_flags.py
+++ src/main/config/command_flags.py
@@ -1,5 +1,13 @@
 from enum import Enum
 
+from src.main.config.command_flag import CommandFlag
+
 
 class CommandFlags(Enum):
-    pass
+    BUFFER_SIZE = CommandFlag('-b', '--buffer-size', 'int'),
+    NUM_PRODUCERS = CommandFlag('-p', '--num-producers', 'int'),
+    NUM_CONSUMERS = CommandFlag('-c', '--num-consumers', 'int'),
+    PRODUCER_WORK_SPEED = CommandFlag('-ps', '--producer-speed-range', 'int:int'),
+    CONSUMER_WORK_SPEED = CommandFlag('-cs', '--consumer-speed-range', 'int:int'),
+    VERBOSE = CommandFlag('-v', '--verbose', ''),
+    SUGGESTIONS = CommandFlag('-s', '--suggestions', ''),
