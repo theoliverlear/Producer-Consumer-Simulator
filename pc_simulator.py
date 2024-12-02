4d357af329d119106124704bf7474419f279283b
theoliverlear <sigwartholiver2@gmail.com>
2024-12-02 16:20:01 -0600

    Add logging setup to main function in pc_simulator.py to enable configurable logging output

--- pc_simulator.py
+++ pc_simulator.py
@@ -2,11 +2,13 @@ import sys
 
 from src.main.config.command_parser import get_config_from_arguments
 from src.main.config.config import Config
+from src.main.logging.setup_logging import setup_logging
 from src.main.simulator.simulator import Simulator
 
 
 def main():
     config: Config = get_config_from_arguments(sys.argv[1:])
+    setup_logging(config.verbose)
     simulator: Simulator = Simulator(config)
     simulator.simulate()
 
