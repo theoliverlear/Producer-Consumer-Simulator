a343ac8593570993009b48fe347f7b9ca2885e64
theoliverlear <sigwartholiver2@gmail.com>
2024-12-02 16:21:42 -0600

    Add num_items_to_process parameter and methods to manage item distribution per producer and consumer in thread_manager.py

--- src/main/thread/thread_manager.py
+++ src/main/thread/thread_manager.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from src.main.buffer.buffer import Buffer
 from src.main.thread.processor.consumer.consumer import Consumer
 from src.main.thread.processor.producer.producer import Producer
@@ -8,20 +10,43 @@ class ThreadManager:
                  num_consumers: int,
                  consumer_speed_range: tuple[int, int],
                  producer_speed_range: tuple[int, int],
-                 buffer: Buffer):
+                 buffer: Buffer,
+                 num_items_to_process: int):
+        self.buffer = buffer
+        self.num_items_to_process = num_items_to_process
         self.num_producers = num_producers
         self.num_consumers = num_consumers
         self.producer_speed_range = producer_speed_range
         self.consumer_speed_range = consumer_speed_range
         self.producers = self.initialize_producers()
         self.consumers = self.initialize_consumers()
-        self.buffer = buffer
+
+    def get_num_items_to_produce(self):
+        items_per_producer: int = self.num_items_to_process // self.num_producers
+        leftover_items: int = self.num_items_to_process % self.num_producers
+        return items_per_producer, leftover_items
+
+    def get_num_items_to_consume(self):
+        items_per_consumer: int = self.num_items_to_process // self.num_consumers
+        leftover_items: int = self.num_items_to_process % self.num_consumers
+        return items_per_consumer, leftover_items
 
     def initialize_producers(self):
-        return [Producer(self.producer_speed_range[0], self.producer_speed_range[1], self.buffer) for _ in range(self.num_producers)]
+        items_per_producer, leftover_items = self.get_num_items_to_produce()
+        producers: List[Producer] = []
+        for i in range(self.num_producers):
+            items_to_produce = items_per_producer + (1 if i < leftover_items else 0)
+            producers.append(Producer(self.producer_speed_range[0], self.producer_speed_range[1], self.buffer, items_to_produce))
+        return producers
+
 
     def initialize_consumers(self):
-        return [Consumer(self.consumer_speed_range[0], self.consumer_speed_range[1], self.buffer) for _ in range(self.num_consumers)]
+        items_per_consumer, leftover_items = self.get_num_items_to_consume()
+        consumers: List[Consumer] = []
+        for i in range(self.num_consumers):
+            items_to_consume = items_per_consumer + (1 if i < leftover_items else 0)
+            consumers.append(Consumer(self.consumer_speed_range[0], self.consumer_speed_range[1], self.buffer, items_to_consume))
+        return consumers
 
 
     def start_producers(self):
