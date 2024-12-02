733aaa62ebbb5a304be0653eac19e712212d8654
theoliverlear <sigwartholiver2@gmail.com>
2024-12-01 18:53:46 -0600

    Add Buffer class in buffer.py to define an abstract memory buffer with enqueue and dequeue methods

---
+++ src/main/buffer/buffer.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+
+class Buffer(ABC):
+    def __init__(self, buffer_size: int):
+        self.buffer_size: int = buffer_size
+        self.is_empty: bool = True
+        self.is_full: bool = False
+
+    @abstractmethod
+    def enqueue(self, number_to_enqueue: int) -> None:
+        pass
+
+    @abstractmethod
+    def dequeue(self) -> int:
+        pass
+
+    @abstractmethod
+    def is_empty(self) -> bool:
+        pass
+
+    @abstractmethod
+    def is_full(self) -> bool:
+        pass
