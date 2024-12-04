f8278d5bb8d52db52fd9f9cfbfe60b471c945fb2
theoliverlear <sigwartholiver2@gmail.com>
2024-12-04 14:42:18 -0600

    Add StatisticTracker dependency to Buffer class in buffer.py for tracking buffer statistics

--- src/main/buffer/buffer.py
+++ src/main/buffer/buffer.py
@@ -1,8 +1,14 @@
 from abc import ABC, abstractmethod
 
+from src.main.statistics.statistic_tracker import StatisticTracker
+
+
 class Buffer(ABC):
-    def __init__(self, buffer_size: int):
+    def __init__(self,
+                 buffer_size: int,
+                 statistic_tracker: StatisticTracker):
         self.buffer_size: int = buffer_size
+        self.statistic_tracker: StatisticTracker = statistic_tracker
         self.is_empty: bool = True
         self.is_full: bool = False
 
