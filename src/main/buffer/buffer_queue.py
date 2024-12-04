3ddeca0781a42ebb17e1eb6b2b96763a3039a4d4
theoliverlear <sigwartholiver2@gmail.com>
2024-12-04 14:44:24 -0600

    Add exception tracking and statistic_tracker to BufferQueue in buffer_queue.py for performance statistics tracking

--- src/main/buffer/buffer_queue.py
+++ src/main/buffer/buffer_queue.py
@@ -4,14 +4,19 @@ from collections import deque
 from src.main.buffer.buffer import Buffer
 from src.main.buffer.empty_buffer_exception import EmptyBufferException
 from src.main.buffer.full_buffer_exception import FullBufferException
-from src.main.statistics.track_performance import track_performance
+from src.main.statistics.statistic_tracker import StatisticTracker
+from src.main.statistics.track_performance import track_performance, \
+    track_exceptions
 from src.main.thread.critical_section import critical_section
 from src.main.thread.processor.lock.mutex_lock import MutexLock
 
 
 class BufferQueue(Buffer):
-    def __init__(self, buffer_size: int, mutex_lock: MutexLock):
-        super().__init__(buffer_size)
+    def __init__(self,
+                 buffer_size: int,
+                 mutex_lock: MutexLock,
+                 statistic_tracker: StatisticTracker):
+        super().__init__(buffer_size, statistic_tracker)
         self.buffer: deque = deque(maxlen=buffer_size)
         self.mutex_lock = mutex_lock
 
@@ -24,6 +29,7 @@ class BufferQueue(Buffer):
 
     @track_performance
     @critical_section
+    @track_exceptions
     def enqueue(self, number_to_enqueue: int) -> None:
         if len(self.buffer) < self.buffer.maxlen:
             self.buffer.appendleft(number_to_enqueue)
@@ -33,6 +39,7 @@ class BufferQueue(Buffer):
 
     @track_performance
     @critical_section
+    @track_exceptions
     def dequeue(self) -> int:
         if len(self.buffer) > 0:
             return self.buffer.pop()
