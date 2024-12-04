e3fda7d55f1b68ce0fb2e5f8b6488eeb82cda925
theoliverlear <sigwartholiver2@gmail.com>
2024-12-04 14:49:29 -0600

    Add StatisticTracker to thread_manager.py for tracking statistics and implement joining of threads

--- src/main/thread/thread_manager.py
+++ src/main/thread/thread_manager.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from src.main.buffer.buffer import Buffer
+from src.main.statistics.statistic_tracker import StatisticTracker
 from src.main.thread.processor.consumer.consumer import Consumer
 from src.main.thread.processor.producer.producer import Producer
 
@@ -11,15 +12,29 @@ class ThreadManager:
                  consumer_speed_range: tuple[int, int],
                  producer_speed_range: tuple[int, int],
                  buffer: Buffer,
-                 num_items_to_process: int):
+                 num_items_to_process: int,
+                 statistic_tracker: StatisticTracker):
         self.buffer = buffer
         self.num_items_to_process = num_items_to_process
         self.num_producers = num_producers
         self.num_consumers = num_consumers
         self.producer_speed_range = producer_speed_range
         self.consumer_speed_range = consumer_speed_range
+        self.statistic_tracker = statistic_tracker
         self.producers = self.initialize_producers()
         self.consumers = self.initialize_consumers()
+
+    def join_all(self):
+        self.join_producers()
+        self.join_consumers()
+
+    def join_producers(self):
+        for producer in self.producers:
+            producer.join()
+
+    def join_consumers(self):
+        for consumer in self.consumers:
+            consumer.join()
 
     def get_num_items_to_produce(self):
         items_per_producer: int = self.num_items_to_process // self.num_producers
@@ -36,7 +51,11 @@ class ThreadManager:
         producers: List[Producer] = []
         for i in range(self.num_producers):
             items_to_produce = items_per_producer + (1 if i < leftover_items else 0)
-            producers.append(Producer(self.producer_speed_range[0], self.producer_speed_range[1], self.buffer, items_to_produce))
+            producers.append(Producer(i + 1,
+                                      self.producer_speed_range[0],
+                                      self.producer_speed_range[1],
+                                      self.buffer, items_to_produce,
+                                      self.statistic_tracker))
         return producers
 
 
@@ -45,7 +64,11 @@ class ThreadManager:
         consumers: List[Consumer] = []
         for i in range(self.num_consumers):
             items_to_consume = items_per_consumer + (1 if i < leftover_items else 0)
-            consumers.append(Consumer(self.consumer_speed_range[0], self.consumer_speed_range[1], self.buffer, items_to_consume))
+            consumers.append(Consumer(i + 1,
+                                      self.consumer_speed_range[0],
+                                      self.consumer_speed_range[1],
+                                      self.buffer, items_to_consume,
+                                      self.statistic_tracker))
         return consumers
 
 
