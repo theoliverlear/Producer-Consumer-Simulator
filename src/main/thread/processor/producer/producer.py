5b89bcf2525e73cb27a07a31feccf047abc553ab
theoliverlear <sigwartholiver2@gmail.com>
2024-12-04 14:46:42 -0600

    Refactor Producer class in producer.py by adding performance tracking and restructuring initialization with statistic_tracker dependency

--- src/main/thread/processor/producer/producer.py
+++ src/main/thread/processor/producer/producer.py
@@ -4,13 +4,26 @@ import time
 
 from src.main.buffer.buffer import Buffer
 from src.main.buffer.full_buffer_exception import FullBufferException
+from src.main.statistics.statistic_tracker import StatisticTracker
+from src.main.statistics.track_performance import track_producer_performance
 from src.main.thread.processor.processor import Processor
 
 
 class Producer(Processor):
-    def __init__(self, speed_floor: int, speed_ceiling: int, buffer: Buffer, num_items_to_process: int):
-        super().__init__(speed_floor, speed_ceiling, buffer, num_items_to_process)
-
+    def __init__(self,
+                 id: int,
+                 speed_floor: int,
+                 speed_ceiling: int,
+                 buffer: Buffer,
+                 num_items_to_process: int,
+                 statistic_tracker: StatisticTracker):
+        super().__init__(id,
+                         speed_floor,
+                         speed_ceiling,
+                         buffer,
+                         num_items_to_process,
+                         statistic_tracker)
+        self.name = f"Producer-{id}"
 
     @staticmethod
     def get_random_number() -> int:
@@ -20,16 +33,20 @@ class Producer(Processor):
     def run(self) -> None:
         self.running = True
         while self.running and self.num_items_to_process > 0:
-            random_number: int = self.get_random_number()
-            try:
-                logging.info(f"Enqueued number: {random_number}")
-                self.buffer.enqueue(random_number)
-                self.num_items_to_process -= 1
-            except FullBufferException:
-                time.sleep(0.01)
-                continue
-            self.simulate_processing()
+            self.process_item()
         self.stop()
+
+    @track_producer_performance
+    def process_item(self) -> None:
+        try:
+            random_number: int = self.get_random_number()
+            logging.info(f"Enqueued number: {random_number}")
+            self.buffer.enqueue(random_number)
+            self.num_items_to_process -= 1
+        except FullBufferException:
+            time.sleep(0.01)
+            return
+        self.simulate_processing()
 
     def stop(self):
         self.running = False
