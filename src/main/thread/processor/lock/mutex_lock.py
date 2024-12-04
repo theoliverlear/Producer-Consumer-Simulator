7e2b3a77872ed15b9ba128c30955538c9c98fc43
theoliverlear <sigwartholiver2@gmail.com>
2024-12-04 15:04:56 -0600

    Refactor mutex_lock.py to remove queue management code that has been migrated to thread_manager.py

--- src/main/thread/processor/lock/mutex_lock.py
+++ src/main/thread/processor/lock/mutex_lock.py
@@ -1,8 +1,7 @@
-import itertools
 import logging
 import threading
 from threading import Condition
-from typing import List
+
 
 from src.main.thread.processor.lock.lock_state import LockState
 
@@ -11,12 +10,6 @@ class MutexLock:
     def __init__(self):
         self.lock_state: LockState = LockState.AVAILABLE
         self.condition: Condition = threading.Condition()
-        self.queue_counter: itertools.count[int] = itertools.count()
-        self.wait_queue: List[int] = []
-
-    def get_next_queue_number(self) -> int:
-        return next(self.queue_counter)
-
 
     def lock(self):
         self.lock_state = LockState.UNAVAILABLE
@@ -27,19 +20,11 @@ class MutexLock:
     def is_locked(self) -> bool:
         return self.lock_state == LockState.UNAVAILABLE
 
-    def is_thread_at_front(self, wait_number: int) -> bool:
-        return len(self.wait_queue) > 0 and self.wait_queue[0] == wait_number
-
-
     def acquire(self) -> None:
         with self.condition:
-            wait_number: int = self.get_next_queue_number()
-            self.wait_queue.append(wait_number)
             thread_name: str = threading.current_thread().name
-            logging.debug(f"Thread {thread_name} requesting the lock with wait number {wait_number}.")
-            while self.is_locked() or not self.is_thread_at_front(wait_number):
+            while self.is_locked():
                 self.condition.wait()
-            self.wait_queue.pop(0)
             self.lock()
             logging.debug(f"Thread {thread_name} acquired the lock.")
 
