c8a66630e714ffce17a69aebc264abf2dc545958
theoliverlear <sigwartholiver2@gmail.com>
2024-12-05 15:08:30 -0600

    Remove synchronized.py as it is no longer needed for thread synchronization functionality

--- src/main/thread/synchronized.py
+++
@@ -1,8 +0,0 @@
-# TODO: Refer to Lutz book to make sure this is correct.
-def synchronized(lock):
-    def decorator(function):
-        def wrapper(*args, **kwargs):
-            with lock:
-                return function(*args, **kwargs)
-        return wrapper
-    return decorator
