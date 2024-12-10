4f3f17e5afefb5634665dc2102ee5dac682b4746
anthanyreth <areth20000@gmail.com>
2024-12-10 16:20:41 -0600

    feat(tests): Add unit tests for BufferQueue functionality
    - Added tests to ensure proper instantiation of the BufferQueue class.
    - Verified buffer value changes using enqueue and dequeue operations.
    - Tested input/output functionality of the buffer queue.
    - Validated correct execution order of enqueue and dequeue operations.
    - Implemented error handling tests for full and empty buffer scenarios.
    - Added logging tests for full and empty buffer states, ensuring proper log messages are generated.

--- src/test/buffer/buffer_queue_test.py
+++ src/test/buffer/buffer_queue_test.py
@@ -1,6 +1,8 @@
 import unittest
 
 from src.main.buffer.buffer_queue import BufferQueue
+from src.main.buffer.empty_buffer_exception import EmptyBufferException
+from src.main.buffer.full_buffer_exception import FullBufferException
 from src.main.statistics.statistic_tracker import StatisticTracker
 from src.main.thread.processor.lock.mutex_lock import MutexLock
 
@@ -17,5 +19,93 @@ class BufferQueueTest(unittest.TestCase):
                                                 statistic_tracker)
         self.assertIsNotNone(buffer_queue)
 
+    def test_buffer_value_changes(self):
+        buffer_size = 10
+        num_items_to_process = 1000
+        mutex_lock = MutexLock()
+        statistic_tracker = StatisticTracker(num_items_to_process)
+        buffer_queue = BufferQueue(buffer_size,
+                                   mutex_lock,
+                                   statistic_tracker)
+        buffer_queue.enqueue(1)
+        self.assertEqual(len(buffer_queue.buffer), 1)
+
+        buffer_queue.dequeue()
+        self.assertEqual(len(buffer_queue.buffer), 0)
+
+    def test_buffer_function_io(self):
+        buffer_size = 10
+        num_items_to_process = 1000
+        mutex_lock = MutexLock()
+        statistic_tracker = StatisticTracker(num_items_to_process)
+        buffer_queue = BufferQueue(buffer_size,
+                                   mutex_lock,
+                                   statistic_tracker)
+
+        buffer_queue.enqueue(1)
+        output = buffer_queue.dequeue()
+        self.assertEqual(output, 1)
+
+    def test_execution_order(self):
+        buffer_size = 10
+        num_items_to_process = 1000
+        mutex_lock = MutexLock()
+        statistic_tracker = StatisticTracker(num_items_to_process)
+        buffer_queue = BufferQueue(buffer_size,
+                                   mutex_lock,
+                                   statistic_tracker)
+
+        buffer_queue.enqueue(1)
+        buffer_queue.enqueue(2)
+        self.assertEqual(buffer_queue.dequeue(), 1)
+        self.assertEqual(buffer_queue.dequeue(), 2)
+
+    def test_error_handling(self):
+        buffer_size = 0
+        num_items_to_process = 1000
+        mutex_lock = MutexLock()
+        statistic_tracker = StatisticTracker(num_items_to_process)
+
+        buffer_queue = BufferQueue(buffer_size, mutex_lock, statistic_tracker)
+
+        with self.assertRaises(FullBufferException):
+            buffer_queue.enqueue(1)
+
+        with self.assertRaises(EmptyBufferException):
+            buffer_queue.dequeue()
+
+    def test_logging_full_buffer(self):
+        buffer_size = 1
+        num_items_to_process = 1000
+        mutex_lock = MutexLock()
+        statistic_tracker = StatisticTracker(num_items_to_process)
+        buffer_queue = BufferQueue(buffer_size,
+                                   mutex_lock,
+                                   statistic_tracker)
+        buffer_queue.enqueue(1)
+
+        with self.assertLogs(level='INFO') as log:
+            with self.assertRaises(FullBufferException):
+                buffer_queue.enqueue(2)
+        self.assertIn("Buffer is full", log.output[0])
+
+    def test_logging_empty_buffer(self):
+        buffer_size = 1
+        num_items_to_process = 1000
+        mutex_lock = MutexLock()
+        statistic_tracker = StatisticTracker(num_items_to_process)
+        buffer_queue = BufferQueue(buffer_size,
+                                   mutex_lock,
+                                   statistic_tracker)
+
+        with self.assertLogs(level='INFO') as log:
+            with self.assertRaises(EmptyBufferException):
+                buffer_queue.dequeue()
+        self.assertIn("Buffer is empty", log.output[0])
+
 if __name__ == '__main__':
     unittest.main()
+
+   
+
+
