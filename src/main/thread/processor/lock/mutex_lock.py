0221a8b85babb7e27dae512b2fce01c637c14141
theoliverlear <sigwartholiver2@gmail.com>
2024-12-12 10:39:34 -0600

    Refactor method order in mutex_lock.py to improve code readability and logical grouping

--- src/main/thread/processor/lock/mutex_lock.py
+++ src/main/thread/processor/lock/mutex_lock.py
@@ -11,15 +11,6 @@ class MutexLock:
         self.lock_state: LockState = LockState.AVAILABLE
         self.condition: Condition = threading.Condition()
 
-    def lock(self):
-        self.lock_state = LockState.UNAVAILABLE
-
-    def unlock(self):
-        self.lock_state = LockState.AVAILABLE
-
-    def is_locked(self) -> bool:
-        return self.lock_state == LockState.UNAVAILABLE
-
     def acquire(self) -> None:
         with self.condition:
             thread_name: str = threading.current_thread().name
@@ -28,19 +19,26 @@ class MutexLock:
             self.lock()
             logging.debug(f"Thread {thread_name} acquired the lock.")
 
-
     def release(self) -> None:
         with self.condition:
             self.unlock()
             logging.debug(f"Thread {threading.current_thread().name} released the lock.")
             self.condition.notify_all()
 
-    def is_available(self) -> bool:
-        return self.lock_state == LockState.AVAILABLE
-
-
     def __enter__(self):
         self.acquire()
 
     def __exit__(self, exception_type, exception_value, exception_traceback):
         self.release()
+
+    def is_available(self) -> bool:
+        return self.lock_state == LockState.AVAILABLE
+
+    def lock(self):
+        self.lock_state = LockState.UNAVAILABLE
+
+    def unlock(self):
+        self.lock_state = LockState.AVAILABLE
+
+    def is_locked(self) -> bool:
+        return self.lock_state == LockState.UNAVAILABLE
