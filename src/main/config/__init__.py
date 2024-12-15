40bd237bf3da042ce4dc4c924b917f3ad27051d2
theoliverlear <sigwartholiver2@gmail.com>
2024-12-15 15:59:04 -0600

    Add PyDoc documentation comments to __init__.py

---
+++ src/main/config/__init__.py
@@ -0,0 +1,7 @@
+"""
+src.main.config package
+
+This package contains all the of data structures and scripts related to the
+configuration of the application. This includes the data structures needed to
+represent the configuration as well as command line argument parsing.
+"""
